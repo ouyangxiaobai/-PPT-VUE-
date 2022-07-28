33157e8485f512d6053bc5a3eff8ed20c9b2ba34
ouyangxiaobai <630155515@qq.com>
2022-07-28 15:41:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# 项目名称
+
+[含论文+辩论PPT+源码等]微信小程序社区疫情防控+后台管理前后分离VUE
+
+# 系统介绍
+开发语言：Java
+框架：ssm
+JDK版本：JDK1.8
+服务器：tomcat7
+数据库：mysql 5.7（一定要5.7版本）
+数据库工具：Navicat11
+开发软件：eclipse/myeclipse/idea
+Maven包：Maven3.3.9
+
+小程序框架：uniapp
+小程序开发软件：HBuilder X
+小程序运行软件：微信开发者
+
+
+
+
+小程序社区疫情防控系统 的系统项目的概述设计分析，进行数据库的是设计，数据采用mysql数据库，并且对于系统的设计采用比较人性化的操作设计，对于系统出现的错误信息可以及时做出处理及反馈。
+
+基于小程序社区疫情防控系统可以在安卓系统的手机上运行，实现，管理员；首页、个人中心、行动轨迹管理、用户管理、体温上报管理、上报提醒管理、疫情公告管理、紧急情况管理、代办管理、防疫物资管理、健康码上传管理、论坛管理、系统管理等功能。方便用户对首页、疫情公告、论坛中心、我的等详细的了解及统计分析。根据系统功能需求建立的模块
+
+# 环境需要
+
+1.运行环境：最好是java jdk 1.8，我们在这个平台上运行的。其他版本理论上也可以。\
+2.IDE环境：IDEA，Eclipse,Myeclipse都可以。推荐IDEA;\
+3.tomcat环境：Tomcat 7.x,8.x,9.x版本均可\
+4.硬件环境：windows 7/8/10 1G内存以上；或者 Mac OS； \
+5.数据库：MySql 5.7版本；\
+6.是否Maven项目：否；
+
+# 技术栈
+
+1. 后端：Spring+SpringMVC+Mybatis\
+2. 前端：JSP+CSS+JavaScript+jQuery
+
+# 使用说明
+
+1. 使用Navicat或者其它工具，在mysql中创建对应名称的数据库，并导入项目的sql文件；\
+2. 使用IDEA/Eclipse/MyEclipse导入项目，Eclipse/MyEclipse导入时，若为maven项目请选择maven;\
+若为maven项目，导入成功后请执行maven clean;maven install命令，然后运行；\
+3. 将项目中springmvc-servlet.xml配置文件中的数据库配置改为自己的配置;\
+4. 运行项目，在浏览器中输入http://localhost:8080/ 登录
+
+# 高清视频演示
+
+https://ym.maptoface.com/archives/46216
+
+
+​
---
+++ components/uni-ui/lib/uni-icons/uni-icons.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/uni-ui/lib/uni-icons/uni-icons"],{"03ab":function(n,t,e){"use strict";e.r(t);var u=e("9a6a"),a=e("9dfc");for(var i in a)"default"!==i&&function(n){e.d(t,n,(function(){return a[n]}))}(i);e("30fd");var c,r=e("f0c5"),f=Object(r["a"])(a["default"],u["b"],u["c"],!1,null,"b56cf54a",null,!1,u["a"],c);t["default"]=f.exports},"30fd":function(n,t,e){"use strict";var u=e("595b"),a=e.n(u);a.a},"595b":function(n,t,e){},"9a6a":function(n,t,e){"use strict";var u,a=function(){var n=this,t=n.$createElement;n._self._c},i=[];e.d(t,"b",(function(){return a})),e.d(t,"c",(function(){return i})),e.d(t,"a",(function(){return u}))},"9dfc":function(n,t,e){"use strict";e.r(t);var u=e("f4a5"),a=e.n(u);for(var i in u)"default"!==i&&function(n){e.d(t,n,(function(){return u[n]}))}(i);t["default"]=a.a},f4a5:function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u=a(e("abad"));function a(n){return n&&n.__esModule?n:{default:n}}var i={name:"UniIcons",props:{type:{type:String,default:""},color:{type:String,default:"#333333"},size:{type:[Number,String],default:16}},data:function(){return{icons:u.default}},methods:{_onClick:function(){this.$emit("click")}}};t.default=i}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/uni-ui/lib/uni-icons/uni-icons-create-component',
+    {
+        'components/uni-ui/lib/uni-icons/uni-icons-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("03ab"))
+        })
+    },
+    [['components/uni-ui/lib/uni-icons/uni-icons-create-component']]
+]);
